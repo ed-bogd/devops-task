19e60744458a564f8e480d6c4afb30887f62b17f
Ed <stegarex@gmail.com>
2025-05-26 00:15:49 +0300

    updated iam

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,6 +1,10 @@
+###
+# eks-admin IAM user and role
+###
+
 # eks-admin IAM user
 resource "aws_iam_user" "eks_admin" {
-  name = "${var.project_name}-eks-admin"
+  name = "eks-admin-user"
   tags = {
     Name = "eks-admin"
   }
@@ -11,25 +15,90 @@ resource "aws_iam_access_key" "eks_admin" {
   user = aws_iam_user.eks_admin.name
 }
 
-# Attach policies to the eks-admin user
-resource "aws_iam_user_policy_attachment" "eks_admin_cluster" {
-  user       = aws_iam_user.eks_admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+# eks-admin IAM role
+resource "aws_iam_role" "eks_admin" {
+  name = "eks-admin"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "eks-admin"
+  }
 }
 
-resource "aws_iam_user_policy_attachment" "eks_admin_service" {
-  user       = aws_iam_user.eks_admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+# Allow the user to assume the role
+resource "aws_iam_user_policy" "eks_admin_assume_role" {
+  name = "eks-admin-assume-role"
+  user = aws_iam_user.eks_admin.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Resource = aws_iam_role.eks_admin.arn
+      }
+    ]
+  })
 }
 
-resource "aws_iam_user_policy_attachment" "eks_admin_node" {
-  user       = aws_iam_user.eks_admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+# Add a custom policy for EKS describe operations
+resource "aws_iam_policy" "eks_admin_policy" {
+  name = "eks-admin-policy"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:*",
+          "ec2:*",
+          "autoscaling:*",
+          "cloudformation:*",
+          "iam:*",
+          "logs:*",
+          "elasticloadbalancing:*",
+          "elasticfilesystem:*",
+          "route53:*",
+          "acm:*",
+          "secretsmanager:*",
+          "ssm:*",
+          "cloudwatch:*",
+          "ecr:*",
+          "kms:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
+
+# Attach the admin policy to the eks-admin role
+resource "aws_iam_role_policy_attachment" "eks_admin_role_policy" {
+  role       = aws_iam_role.eks_admin.name
+  policy_arn = aws_iam_policy.eks_admin_policy.arn
+}
+
+###
+# eks-readonly IAM user
+###
 
 # eks-readonly IAM role
 resource "aws_iam_role" "eks_read_only" {
-  name = "${var.project_name}-eks-read-only"
+  name = "eks-read-only"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -46,7 +115,54 @@ resource "aws_iam_role" "eks_read_only" {
 }
 
 # Attach policies to the eks-readonly role
-resource "aws_iam_role_policy_attachment" "eks_read_only_attach" {
+# Custom read-only policy for EKS using wildcards
+resource "aws_iam_policy" "eks_read_only_policy" {
+  name        = "eks-read-only-policy"
+  description = "Read-only access to EKS cluster using wildcards"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:Describe*",
+          "eks:List*",
+          "ec2:Describe*",
+          "autoscaling:Describe*",
+          "cloudformation:Describe*",
+          "cloudformation:List*",
+          "iam:List*",
+          "iam:Get*",
+          "logs:Describe*",
+          "logs:Get*",
+          "logs:Filter*",
+          "elasticloadbalancing:Describe*",
+          "elasticfilesystem:Describe*",
+          "route53:List*",
+          "route53:Get*",
+          "acm:List*",
+          "acm:Describe*",
+          "secretsmanager:Describe*",
+          "secretsmanager:List*",
+          "ssm:Describe*",
+          "cloudwatch:Get*",
+          "cloudwatch:List*",
+          "cloudwatch:Describe*",
+          "ecr:Describe*",
+          "ecr:List*",
+          "ecr:Get*",
+          "kms:Describe*",
+          "kms:List*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the custom read-only policy to the eks-readonly role
+resource "aws_iam_role_policy_attachment" "eks_read_only_custom" {
   role       = aws_iam_role.eks_read_only.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = aws_iam_policy.eks_read_only_policy.arn
 }
