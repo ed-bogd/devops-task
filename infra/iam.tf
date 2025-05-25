df97e69833bc9b8a17af166dda8d464f56b4fcc5
Ed <stegarex@gmail.com>
2025-05-26 02:24:21 +0300

    updated iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -97,105 +97,105 @@ resource "aws_iam_role_policy_attachment" "eks_admin_role_policy" {
 ###
 
 # eks-readonly IAM user
-resource "aws_iam_user" "eks_read_only" {
-  name = "eks-read-only-user"
-  tags = {
-    Name = "eks-read-only"
-  }
-}
+# resource "aws_iam_user" "eks_read_only" {
+#   name = "eks-read-only-user"
+#   tags = {
+#     Name = "eks-read-only"
+#   }
+# }
 
-# Create an IAM access key for the eks-read-only user
-resource "aws_iam_access_key" "eks_read_only" {
-  user = aws_iam_user.eks_read_only.name
-}
+# # Create an IAM access key for the eks-read-only user
+# resource "aws_iam_access_key" "eks_read_only" {
+#   user = aws_iam_user.eks_read_only.name
+# }
 
-# eks-readonly IAM role
-resource "aws_iam_role" "eks_read_only" {
-  name = "eks-read-only"
+# # eks-readonly IAM role
+# resource "aws_iam_role" "eks_read_only" {
+#   name = "eks-read-only"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
 
-  tags = {
-    Name = "eks-read-only"
-  }
-}
+#   tags = {
+#     Name = "eks-read-only"
+#   }
+# }
 
-# Allow the read-only user to assume the read-only role
-resource "aws_iam_user_policy" "eks_read_only_assume_role" {
-  name = "eks-read-only-assume-role"
-  user = aws_iam_user.eks_read_only.name
+# # Allow the read-only user to assume the read-only role
+# resource "aws_iam_user_policy" "eks_read_only_assume_role" {
+#   name = "eks-read-only-assume-role"
+#   user = aws_iam_user.eks_read_only.name
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRole"
-        Resource = aws_iam_role.eks_read_only.arn
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = "sts:AssumeRole"
+#         Resource = aws_iam_role.eks_read_only.arn
+#       }
+#     ]
+#   })
+# }
 
-# Custom read-only policy for EKS using wildcards
-resource "aws_iam_policy" "eks_read_only_policy" {
-  name        = "eks-read-only-policy"
-  description = "Read-only access to EKS cluster using wildcards"
+# # Custom read-only policy for EKS using wildcards
+# resource "aws_iam_policy" "eks_read_only_policy" {
+#   name        = "eks-read-only-policy"
+#   description = "Read-only access to EKS cluster using wildcards"
   
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "eks:Describe*",
-          "eks:List*",
-          "ec2:Describe*",
-          "autoscaling:Describe*",
-          "cloudformation:Describe*",
-          "cloudformation:List*",
-          "iam:List*",
-          "iam:Get*",
-          "logs:Describe*",
-          "logs:Get*",
-          "logs:Filter*",
-          "elasticloadbalancing:Describe*",
-          "elasticfilesystem:Describe*",
-          "route53:List*",
-          "route53:Get*",
-          "acm:List*",
-          "acm:Describe*",
-          "secretsmanager:Describe*",
-          "secretsmanager:List*",
-          "ssm:Describe*",
-          "cloudwatch:Get*",
-          "cloudwatch:List*",
-          "cloudwatch:Describe*",
-          "ecr:Describe*",
-          "ecr:List*",
-          "ecr:Get*",
-          "kms:Describe*",
-          "kms:List*"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "eks:Describe*",
+#           "eks:List*",
+#           "ec2:Describe*",
+#           "autoscaling:Describe*",
+#           "cloudformation:Describe*",
+#           "cloudformation:List*",
+#           "iam:List*",
+#           "iam:Get*",
+#           "logs:Describe*",
+#           "logs:Get*",
+#           "logs:Filter*",
+#           "elasticloadbalancing:Describe*",
+#           "elasticfilesystem:Describe*",
+#           "route53:List*",
+#           "route53:Get*",
+#           "acm:List*",
+#           "acm:Describe*",
+#           "secretsmanager:Describe*",
+#           "secretsmanager:List*",
+#           "ssm:Describe*",
+#           "cloudwatch:Get*",
+#           "cloudwatch:List*",
+#           "cloudwatch:Describe*",
+#           "ecr:Describe*",
+#           "ecr:List*",
+#           "ecr:Get*",
+#           "kms:Describe*",
+#           "kms:List*"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
 
-# Attach the custom read-only policy to the eks-readonly role
-resource "aws_iam_role_policy_attachment" "eks_read_only_custom" {
-  role       = aws_iam_role.eks_read_only.name
-  policy_arn = aws_iam_policy.eks_read_only_policy.arn
-}
+# # Attach the custom read-only policy to the eks-readonly role
+# resource "aws_iam_role_policy_attachment" "eks_read_only_custom" {
+#   role       = aws_iam_role.eks_read_only.name
+#   policy_arn = aws_iam_policy.eks_read_only_policy.arn
+# }
